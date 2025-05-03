42b5696184d1963370a5b03b942e724df1fdee19
thurpe <babatope@pressone.co>
2025-05-03 23:24:14 +0100

    Added fe endpoint

--- src/main/java/com/web/activity_tracker/config/SecurityConfig.java
+++ src/main/java/com/web/activity_tracker/config/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
